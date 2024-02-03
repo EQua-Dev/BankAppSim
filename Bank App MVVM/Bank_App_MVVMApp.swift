b0e5017c29e49c881c2901577b09839ca5b16bf3
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-02-03 18:12:39 +0100

    Initial Commit

---
+++ Bank App MVVM/Bank_App_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bank_App_MVVMApp.swift
+//  Bank App MVVM
+//
+//  Created by Richard Uzor on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Bank_App_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
