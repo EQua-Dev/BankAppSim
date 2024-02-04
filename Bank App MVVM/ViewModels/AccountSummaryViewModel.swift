9eb23237a002617dfb4aab409d7abfbc9801de9e
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-02-04 12:28:34 +0100

    account summary viewmodel implemented

---
+++ Bank App MVVM/ViewModels/AccountSummaryViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  AccountSummaryViewModel.swift
+//  Bank App MVVM
+//
+//  Created by Richard Uzor on 04/02/2024.
+//
+
+import Foundation
+
+class AccountSummaryViewModel: ObservableObject{
+    
+    private var _accounts = [Account]()
+    
+    var accounts: [AccountViewModel] = [AccountViewModel]()
+    
+    //get the total of the account summary by mapping through all the accounts and adding the balance
+    var total: Double{
+        _accounts.map {$0.balance}.reduce(0, +)
+    }
+    
+    func getAllAccounts(){
+        AccountService.shared.getAllAccounts{ result in
+            switch result {
+                case .success(let accounts):
+                    if let accounts = accounts{
+                        self._accounts = accounts
+                        self.accounts = accounts.map(AccountViewModel.init)
+                    }
+                    print("success")
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+            
+        }
+    }
+    
+}
+
+class AccountViewModel {
+    
+    var account: Account
+    
+    init(account: Account) {
+        self.account = account
+    }
+    
+    //expose the various properties of the  account model
+    
+    var name: String{
+        account.name
+    }
+    
+    var accountId: String{
+        account.id.uuidString
+    }
+    
+    var accountType: String{
+        account.accountType.title
+    }
+
+    var balance: Double{
+        account.balance
+    }
+    
+}
