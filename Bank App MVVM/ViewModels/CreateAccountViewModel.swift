34f5cad90139a396a3a43270072607acb4ee3516
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-02-07 06:45:06 +0100

    create account viewmodel created with input validation

---
+++ Bank App MVVM/ViewModels/CreateAccountViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  CreateAccountViewModel.swift
+//  Bank App MVVM
+//
+//  Created by Richard Uzor on 07/02/2024.
+//
+
+import Foundation
+
+
+class CreateAccountViewModel: ObservableObject {
+    
+    var name: String = ""
+    var accountType: AccountType = .checking
+    var balance: String = ""
+    @Published var errorMessage: String = ""
+    
+}
+
+extension CreateAccountViewModel{
+    //input validation
+    private var isNameValid: Bool {
+        !name.isEmpty
+    }
+    private var isBalanceValid: Bool {
+        guard let userBalance = Double(balance) else {
+            return false
+        }
+        return userBalance <= 0 ? false : true
+    }
+    
+    private func isValid() -> Bool{
+        var errors = [String]()
+        
+        if !isNameValid{
+            errors.append("Name is not valid")
+        }
+        if !isBalanceValid{
+            errors.append("Balance is not valid")
+        }
+        if !errors.isEmpty {
+            self.errorMessage = errors.joined(separator: "\n")
+            return false
+        }
+        
+        return true
+    }
+}
+
+extension CreateAccountViewModel{
+    
+    func createAccount(completion: @escaping (Bool) -> Void){
+        
+        if !isValid() {return completion(false)}
+        let createAccountReq = CreateAccountRequest(name: name, accountType: accountType.rawValue, balance: Double(balance)!)
+        
+        AccountService.shared.createAccount(createAccountRequest: createAccountReq){ result in
+//            switch result {
+//                case .success(createAccountResponse):
+//
+//                case .failure(error):
+//
+//            }
+            
+        }
+    }
+    
+}
