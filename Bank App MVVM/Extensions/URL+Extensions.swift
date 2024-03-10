5ab276824e8e384da48f17ef872a52477b1647ca
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2024-03-10 16:28:44 +0100

    updated the url routes

--- Bank App MVVM/Extensions/URL+Extensions.swift
+++ Bank App MVVM/Extensions/URL+Extensions.swift
@@ -6,7 +6,10 @@
 //
 
 import Foundation
+var baseUrl = "http://0.0.0.0:8080/"
 extension URL {
+    
+   
     
     static func urlForAccounts() -> URL? {
          return URL(string: "https://wood-pushy-buckthornpepperberry.glitch.me/api/accounts")
@@ -19,4 +22,33 @@ extension URL {
     static func urlForTransferFunds() -> URL? {
         return URL(string: "https://wood-pushy-buckthornpepperberry.glitch.me/api/transfer")
     }
+    
+    static func urlForSignUp() -> URL? {
+        return URL(string: "\(baseUrl)signup")
+    }
+    
+    static func urlForSignIn() -> URL? {
+        return URL(string: "\(baseUrl)signin")
+    }
+    
+    static func urlForCreateAccount() -> URL? {
+        return URL(string: "\(baseUrl)create-account")
+    }
+    
+    static func urlForFetchAllAccountsOfUser() -> URL? {
+        return URL(string: "\(baseUrl)my-accounts")
+    }
+    
+    static func urlForFetchAccountDetails(accountNo: String) -> URL? {
+        return URL(string: "\(baseUrl)find-account/\(accountNo)")
+    }
+    
+    static func urlForTransferFunds(myAccountNo: String) -> URL? {
+        return URL(string: "\(baseUrl)transfer-funds/\(myAccountNo)")
+    }
+    
+    static func urlForFetchTransferHistory() -> URL? {
+        return URL(string: "\(baseUrl)my-transfer-history")
+    }
+    
 }
---
+++ Bank App MVVM/Models/CreateUserRequest.swift
@@ -0,0 +1,13 @@
+//
+//  CreateUserRequest.swift
+//  Bank App MVVM
+//
+//  Created by Richard Uzor on 10/03/2024.
+//
+
+import Foundation
+
+struct CreateUserRequest: Codable{
+    let username: String
+    let password: String
+}
